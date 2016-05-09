4f94640b10975ae5b98951d901d2213d4480fddf
salvesvence <salvesvence@gmail.com>
2016-05-09 10:56:53 +0200

    We created the gulpfile.js file with its first configuration.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('sav-select.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['scripts']);
