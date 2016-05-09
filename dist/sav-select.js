8d3dd483d1878f09a7389e22dd62eaaf11064040
salvesvence <salvesvence@gmail.com>
2016-05-09 16:44:16 +0200

    We added the initial content into the sav-select.js file.

--- dist/sav-select.js
+++ dist/sav-select.js
@@ -1,10 +1,53 @@
-(function() {
+// Utility
+if( typeof  Object.create !== 'function' ) {
+    Object.create = function( obj ) {
+        function F(){};
+        F.prototype = obj;
+        return new F();
+    }
+}
+
+(function($, window, document, undefined) {
 
     var Select = {
+        init: function( options, elem ) {
+            var self = this;
 
-        init: function() {
+            self.elem = elem;
+            self.$elem = $(elem);
 
+            if( typeof options === 'string' ) {
+                self.search = options;
+            } else {
+                // object was passed
+                self.search = options.search;
+                self.options = $.extend( {}, $.fn.SavSelect.options, options );
+            }
+
+            self.options();
+        },
+
+        options: function() {
+            var self = this;
+            var options = [];
+
+            self.$elem.find('option').each( function() {
+                options.push( $(this).val() );
+            });
+
+            return options;
         }
     };
 
-})();
+    $.fn.SavSelect = function( options ) {
+        return this.each( function() {
+            var select = Object.create( Select );
+            select.init( options, this );
+        });
+    };
+
+    $.fn.SavSelect.options = {
+        search: 'sav'
+    };
+
+})(jQuery, window, document);
